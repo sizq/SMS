7f6c67f6bf667221f3240a18b64ebb02ac4487ab
sizq <49854063+sizq@users.noreply.github.com>
2022-07-15 10:37:14 +0800

    Update SMSUtil.java

--- SMS/src/main/java/soft/develop/project/util/SMSUtil.java
+++ SMS/src/main/java/soft/develop/project/util/SMSUtil.java
@@ -2,7 +2,7 @@ package soft.develop.project.util;
 
 public class SMSUtil {
 	public static int APPID = 1400203120;
-	public static String APPKEY = "8c676640ddae13ef4c73195205fa5faa";
-	public static int TEMPLATEID = 318343;
+	public static String APPKEY = "Tencent Key";
+	public static int TEMPLATEID = TEMPLATEID;
 	public static String SMSSIGN = "司智琪日常分享";
 }
